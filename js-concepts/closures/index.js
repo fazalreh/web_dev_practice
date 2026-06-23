31575d93e46a60036e0eaa502398eefd8b55d45d
fazalreh <27100294@lums.edu.pk>
2026-06-23 20:19:02 +0500

    practicing closures

---
+++ js-concepts/closures/index.js
@@ -0,0 +1,18 @@
+
+
+function createCounter() {
+  let count = 0;
+
+  return function increment() {
+    count += 1;
+    return count;
+  };
+}
+
+const counter = createCounter();
+
+console.log(counter());
+console.log(counter());
+console.log(counter()); 
+console.log(counter()); 
+
